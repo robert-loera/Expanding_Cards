164e585c40ca2470e6950650a566e74608772533
robert-loera <82541496+robert-loera@users.noreply.github.com>
2021-09-27 15:50:58 -0500

    expanding cards complete

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const x = 'test'
+//expanding effect
+const panels = document.querySelectorAll('.panel')
+
+//when panel is clicked
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
